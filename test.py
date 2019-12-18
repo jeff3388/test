627439fb9b67c891f21ac3675daad3fbb5c48230
jojo <jaxlouder@gmail.com>
2019-12-19 01:25:31 +0800

    123

--- test.py
+++ test.py
@@ -1,47 +1,14 @@
-import pandas as pd
+ls = []
+league = ["league_1","league_2"]
+team = [{"test1":["team1","na","na","na"]},{"test2":["team2","na","na"]}]
 
-colum1 = ['1', '', '3', '', '4']
-colum2 = ['22', '', '22', '', '66']
-colum3 = ['0.8', '0.7', '0.9', '0.3', '1.2']
-colum4 = ['1.3', '1.1', '1.5', '1.4', '1.8']
+for i ,j in zip(league,team):
+    ls.append({i:j})
 
-df = pd.DataFrame({'team1': colum1,'team2': colum2,
-                   'home_odds': colum3,'away_odds': colum4})
-df
-
-
-team_list = df.values.tolist()
-en_ls = list(enumerate(team_list))
-
-dit1 = {}
-for e in en_ls:
-    d1 = {e[0]: e[1]}
-    dit1.update(d1)
-
-print(dit1)
-
-
-team_list = df.values.tolist()
-en_ls = list(enumerate(team_list))
-
-dit1 = {}
-for e in en_ls:
-    d1 = {e[0]: e[1]}
-    dit1.update(d1)
-    
-new_ls = [i for i in en_ls if i[1][1] == '']
-
-dit2 = {}
-for e in new_ls:
-    d2 = {e[0]: e[1]}
-    dit2.update(d2)
-
-correct_ls = []
-for r in dit2:
-    val1 = dit2.get(r)[2:]
-    val2 = dit1.get(r-1)
-    
-    new_val = val2 + val1
-    correct_ls += [new_val]
-    
-print(correct_ls) 
+sob_match_dict = {"league":{
+                    "home": {"0.8":"46816516"},
+                    "away": {"0.8":"51351351"},
+                    "over": {"0.8":"61351551"},
+                    "under": {"0.8":"46546546"}
+                           }
+                 }
